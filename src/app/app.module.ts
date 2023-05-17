0f142440e842e6d24ec3562cf34ea577350c7877
Ekapsos <sitapaslesamtalesum@gmail.com>
2023-05-17 17:41:30 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ROUTES } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    SignUpComponent,
+    UserProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
